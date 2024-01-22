afe11e418af11f9061d2206b93c1c65c99ecedef
Gabriel Amirati <gabriel_amirati@hotmail.com>
2024-01-22 19:25:31 -0300

    Tela de login completa

---
+++ src/components/Login.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com)$/;
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const valid = !(emailRegex.test(email) && password.length > 6);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const saveOnLS = {
+      email,
+    };
+    localStorage.setItem('user', JSON.stringify(saveOnLS));
+    navigate('/meals');
+  };
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <label htmlFor="emailInput">
+        Email:
+        {' '}
+        <input
+          data-testid="email-input"
+          id="emailInput"
+          type="email"
+          value={ email }
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+      </label>
+      {' '}
+      <label htmlFor="passwordInput">
+        Password:
+        {' '}
+        <input
+          data-testid="password-input"
+          id="passwordInput"
+          type="password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+      </label>
+      {' '}
+      <input
+        id="submit-Btn"
+        value="Enter"
+        data-testid="login-submit-btn"
+        type="submit"
+        disabled={ valid }
+      />
+    </form>
+  );
+}
+
+export default Login;
