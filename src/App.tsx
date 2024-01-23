f559f6b8dd14abeb2c066089b3efa397c48c2409
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2024-01-22 23:47:40 -0300

    De 15 validações, passei em 8. Apenas o requisito 08 está 100%

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="/meals/*" element={ <Header /> } />
+      <Route path="/drinks/*" element={ <Header /> } />
+      <Route path="/meals/:id/*" />
+      <Route path="/drinks/:id/*" />
+      <Route path="/profile" element={ <Header /> } />
+      <Route path="/done-recipes" element={ <Header /> } />
+      <Route path="/favorite-recipes" element={ <Header /> } />
     </Routes>
   );
 }
