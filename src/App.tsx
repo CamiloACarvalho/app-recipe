8d2fe3d42cf92b3dcfcf6a80f6b68721f4d216ff
Gabriel Amirati <gabriel_amirati@hotmail.com>
2024-01-23 20:19:46 -0300

    Footer completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Login from './components/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/meals/*" element={ <Header /> } />
-      <Route path="/drinks/*" element={ <Header /> } />
+      <Route
+        path="/meals/*"
+        element={
+          <>
+            <Header />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/drinks/*"
+        element={
+          <>
+            <Header />
+            <Footer />
+          </>
+        }
+      />
       <Route path="/meals/:id/*" />
       <Route path="/drinks/:id/*" />
       <Route path="/meals/:id/in-progress/*" />
       <Route path="/drinks/:id/in-progress/*" />
-      <Route path="/profile" element={ <Header /> } />
+      <Route
+        path="/profile"
+        element={
+          <>
+            <Header />
+            <Footer />
+          </>
+        }
+      />
       <Route path="/done-recipes" element={ <Header /> } />
       <Route path="/favorite-recipes" element={ <Header /> } />
     </Routes>
