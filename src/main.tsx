9a6d187938fc51555d653b7244c3a40ab6fb300e
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2024-01-24 19:30:15 -0300

    preparando para fazer merge

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import SearchProvider from './context/SearchContext/SearchProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <BrowserRouter>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </BrowserRouter>,
   );
