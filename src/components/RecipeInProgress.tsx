9b227d76ce5aa5c81355bde9a9275031e7fa2559
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2024-01-29 20:56:11 -0300

    criando nova branch

--- src/components/RecipeInProgress.tsx
+++ src/components/RecipeInProgress.tsx
@@ -23,7 +23,7 @@ function Recipes() {
       .then((data) => {
         const recipeType = mealPage ? 'meals' : 'drinks';
         const fetchedRecipes = data[recipeType];
-        setRecipes(fetchedRecipes); // Aqui tenho que pegar a informação dos itens apenas em progresso para renderizar nessa tela nessa tela
+        setRecipes(fetchedRecipes);
         setConcludedRecipe(new Array(fetchedRecipes.length).fill(false));
       });
   }, [setRecipes, location.pathname, id]);
@@ -37,6 +37,56 @@ function Recipes() {
   };
 
   const enableFinishButton = concludedRecipe.every((value) => value);
+
+  const handleToggleFavorite = () => {
+    setFavorite(!favorite);
+    // Preciso criar outro componente de FavoriteRecipes para complementar essa função
+  };
+
+  const handleShareLinkRecipeInProgress = () => {
+    const mealPage = location.pathname.split('/')[1] === 'meals';
+    const recipeType = mealPage ? 'meals' : 'drinks';
+    const recipeId = location.pathname.split('/')[2];
+    const link = `http://localhost:3000/${recipeType}/${recipeId}`;
+
+    navigator.clipboard.writeText(link)
+      .then(() => {
+        console.log('Link copiado com sucesso!');
+      })
+      .catch((error) => {
+        console.error('Erro ao copiar o link:', error);
+      });
+  };
+
+  const handleFineshedRecipe = () => {
+    const mealPage = location.pathname.split('/')[1] === 'meals';
+    const endpoint = mealPage
+      ? mealEndpoint
+      : drinkEndpoint;
+    fetch(`${endpoint}${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const recipeType = mealPage ? 'meals' : 'drinks';
+        const fetchedRecipes = data[recipeType];
+        const recipe = fetchedRecipes[0];
+        const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes') || '[]');
+        const newDoneRecipe = {
+          id: recipe[`id${recipeType.slice(0, 1).toUpperCase()}${recipeType.slice(1)}`],
+          type: recipeType.slice(0, -1),
+          area: recipe.strArea || '',
+          category: recipe.strCategory || '',
+          alcoholicOrNot: recipe.strAlcoholic || '',
+          name: recipe[`str${recipeType.slice(0, 1)
+            .toUpperCase()}${recipeType.slice(1)}`],
+          image: recipe[`str${recipeType.slice(0, 1)
+            .toUpperCase()}${recipeType.slice(1)}Thumb`],
+          doneDate: new Date().toLocaleDateString('pt-BR'),
+          tags: recipe.strTags ? recipe.strTags.split(',') : [],
+        };
+        doneRecipes.push(newDoneRecipe);
+        localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+      });
+  };
 
   return (
     <main>
@@ -54,11 +104,10 @@ function Recipes() {
           ))
         }
       </div>
-      {/* Desobrir pq os ícones não estão aparecendo */}
       <button
         type="button"
         data-testid="favorite-btn"
-        onClick={ () => setFavorite(!favorite) }
+        onClick={ handleToggleFavorite }
       >
         <img
           { ...favorite
@@ -71,6 +120,7 @@ function Recipes() {
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ handleShareLinkRecipeInProgress }
       >
         <img
           src="src/images/shareIcon.svg"
@@ -80,7 +130,7 @@ function Recipes() {
       <button
         type="button"
         disabled={ !enableFinishButton }
-        onClick={ () => console.log('receita concluída') }
+        onClick={ handleFineshedRecipe }
       >
         <img
           src="src/images/icons8-done.svg"
