34fb13abd025116e6a05037c490e69a743b83746
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2024-02-01 13:40:31 -0300

    criando testes

--- src/tests/06.RecipeInProgress.test.tsx
+++ src/tests/06.RecipeInProgress.test.tsx
@@ -1,11 +1,12 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import App from '../App';
 import SearchProvider from '../context/SearchContext/SearchProvider';
 import { renderWithRouter } from '../utils/renderWithRouter';
 import OneDrinkMock from './mocks/OneDrinkMock';
 
-describe('Testa o componente Header', async () => {
+describe('Teste para o componente Recipe In Progress', async () => {
   beforeEach(() => vi
     .spyOn(global, 'fetch')
     .mockResolvedValue({
@@ -34,5 +35,43 @@ describe('Testa o componente Header', async () => {
 
     const image = await screen.findByTestId('recipe-photo');
     expect(image).toBeInTheDocument();
+
+    const itHasAlchool = await screen.findByRole('heading', { name: /alcoholic/i });
+    expect(itHasAlchool).toHaveTextContent('Alcoholic');
+
+    const ingredient01 = await screen.findByTestId('0-ingredient-step');
+    expect(ingredient01).toHaveTextContent('Hpnotiq: 2 oz');
+
+    const ingredient02 = await screen.findByTestId('1-ingredient-step');
+    expect(ingredient02).toHaveTextContent('Pineapple Juice: 1 oz');
+
+    const ingredient03 = await screen.findByTestId('2-ingredient-step');
+    expect(ingredient03).toHaveTextContent('Banana Liqueur: 1 oz');
+
+    const preparation = await screen.findByTestId('instructions');
+    expect(preparation).toBeInTheDocument();
+
+    const video = await screen.findByTestId('video');
+    expect(video).toBeInTheDocument();
+
+    const favoriteButton = await screen.findByTestId('favorite-btn');
+    expect(favoriteButton).toBeInTheDocument();
+
+    const shareButton = await screen.findByTestId('share-btn');
+    expect(shareButton).toBeInTheDocument();
+
+    const finshButton = await screen.findByTestId('finish-recipe-btn');
+    expect(finshButton).toBeInTheDocument();
+  });
+
+  test('02 - Verificando as funcionalidades da tela "Recipes in Progress"', async () => {
+    renderWithRouter(
+      <SearchProvider>
+        <App />
+      </SearchProvider>,
+      { route: '/drinks/178319/in-progress' },
+    );
+
+    
   });
 });
